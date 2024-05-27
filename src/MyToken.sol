ec0e840ed1096675f296051ad294c6a3abe8707b
Tran Minh Tuan <tuan.tm214978@sis.hust.edu.vn>
2024-05-27 15:56:34 +0700

    update
    ERC20.sol: Manages AToken generation and burning based on user deposits and withdrawals from the lending pool
    
     MyToken.sol: Generates our own currency which has symbol IBT which we are minting to users accounts and is used for transactions as a second cryptocurrency (testing)

---
+++ src/MyToken.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.13;
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ECR20{
+    address public lendContractAddress; 
+
+    constructor() ERC20("MyToken", "IBT"){
+    }
+
+    function mintTokenswithMTC(address recipient) external {
+        _mint(recipient, 10000000 * (10 ** 18)); 
+
+    }
+
+    function setLendContractAddress(address _lendContractAddress) external {
+        lendContractAddress = _lendContractAddress; 
+    }
+
+    function approveLendContract(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "Invalid spender address"); 
+        require(amount <= balanceOf(msg.sender), "Insufficient balance");
+        require(amount <= allowance(msg.sender, spender), "Amount exceeds balance"); 
+
+        _aprrove(msg.sender, spender, amount);
+
+        return true; 
+    }
+}
